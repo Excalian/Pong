a62c6015fe412455e417cc8eb288feacb3172982
Marcellino Abdelmalak <me@excalian.com>
2026-04-05 00:31:21 -0700

    Added a flash to each time you score, and also made the background black, and also added a bit of color to the goals to show they are the goals

--- paddle/paddle.gd
+++ paddle/paddle.gd
@@ -5,15 +5,25 @@ const AI_REACTIVITY: int = 900
 const AI_DEADZONE: float = 15.0
 
 @export var ball_system: BallSystem
+@export var goal_system: GoalSystem
 @export var paddle_binding: PaddleBinding
 @export var speed: float = 50.0
+@export var flash_shader: Shader
 
+@onready var paddle_color: Color = paddle_binding.player_data.paddle_color
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite_material: ShaderMaterial = ShaderMaterial.new()
 
-var ball_node_path: NodePath
+var current_tween: Tween
 
 
 func _ready() -> void:
-	modulate = paddle_binding.player_data.paddle_color
+	modulate = paddle_color
+	goal_system.player_won.connect(_on_player_won)
+	
+	sprite_material.shader = flash_shader
+	sprite_material.set_shader_parameter("flash_strength", 0.0)
+	sprite.material = sprite_material
 
 func _physics_process(delta: float) -> void:
 	var target_direction: float = 0.0
@@ -32,6 +42,25 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 0
 	
 	move_and_slide()
+
+
+func _on_player_won(_ball: Ball, player: PlayerData) -> void:
+	if player != paddle_binding.player_data:
+		return
+	
+	flash()
+
+
+func flash() -> void:
+	if current_tween:
+		current_tween.kill()
+	
+	current_tween = create_tween()
+	current_tween.set_trans(Tween.TRANS_QUAD)
+	
+	current_tween.tween_property(sprite_material, "shader_parameter/flash_strength", 1.0, 0.05)
+	current_tween.tween_property(sprite_material, "shader_parameter/flash_strength", 0.6, 0.08)
+	current_tween.tween_property(sprite_material, "shader_parameter/flash_strength", 0.0, 0.35)
 
 
 func _get_direction() -> float:
--- scripts/goal.gd
+++ scripts/goal.gd
@@ -3,3 +3,12 @@ class_name Goal
 
 
 @export var points_to_give: int = 1
+
+func _ready() -> void:
+	var rect_size: Vector2 = $CollisionShape2D.shape.size 
+	var rect: ColorRect = ColorRect.new()
+	rect.color = Color.WHITE.darkened(0.95)
+	rect.position = rect_size / -2
+	rect.size = rect_size
+
+	add_child(rect)
