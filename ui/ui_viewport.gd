18d2fbaf581132f4cdb4e401aa078e25d7b7d280
Marcellino Abdelmalak <me@excalian.com>
2026-04-06 09:19:36 -0700

    v1.2
    fixed the html export (Maybe)

--- ui/ui_viewport.gd
+++ ui/ui_viewport.gd
@@ -4,7 +4,7 @@ extends SubViewportContainer
 
 func _ready() -> void:
 	_resize_ui_viewport_container()
-	sub_viewport.size_changed.connect(_resize_ui_viewport_container)
+	get_viewport().size_changed.connect(_resize_ui_viewport_container)
 
 func _resize_ui_viewport_container() -> void:
 	size = get_viewport().get_visible_rect().size
